8ddf3a2cf9ffd1275a59ee1fc943920a8d410ffc
cumet04 <medalhkr@gmail.com>
2020-07-30 22:02:12 +0900

    login accept args with prompt

--- cmd/login.go
+++ cmd/login.go
@@ -2,27 +2,52 @@ package cmd
 
 import (
 	"atcoder-gli/atcoder"
-	"context"
+	"bufio"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func init() {
 	rootCmd.AddCommand(
 		&cobra.Command{
-			Use:   "login",
+			// TODO: argsの説明（省略したらプロンプトになるなど）を入れたい
+			Use:   "login [username] [password]",
 			Short: "login to AtCoder",
-			Args:  cobra.ExactArgs(2),
+			Args:  cobra.MaximumNArgs(2),
 			Run:   runLogin,
 		})
 }
 
 func runLogin(cmd *cobra.Command, args []string) {
-	ctx := context.Background()
-	ac := atcoder.NewAtCoder(ctx, "")
-	cookie, err := ac.Login(args[0], args[1])
+	var user string
+	var pass string
+	if len(args) >= 1 {
+		user = args[0]
+	}
+	if len(args) >= 2 {
+		pass = args[1]
+	}
+	if user == "" {
+		fmt.Print("Enter Username: ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		user = scanner.Text()
+	}
+	if pass == "" {
+		fmt.Print("Enter Password: ")
+		bytes, err := terminal.ReadPassword(0)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("")
+		pass = string(bytes)
+	}
+
+	ac := atcoder.NewAtCoder(cmd.Context(), "")
+	cookie, err := ac.Login(user, pass)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/pkg/errors v0.8.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.4.0
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 )
